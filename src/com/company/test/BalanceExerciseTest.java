94a73a2f194aa2c137f45563406b3a746d5a11b1
Andrea Meyer <an-dr-ea93@hotmail.com>
2020-04-21 12:39:28 +0200

    New comment

--- src/com/company/test/BalanceExerciseTest.java
+++ src/com/company/test/BalanceExerciseTest.java
@@ -24,6 +24,7 @@ public class BalanceExerciseTest {
     @Test
     public void test1_2() {
 
+        // Testing the getType(), to see if it returns the correct ExerciseType
         BalanceExercise balanceExercise = new BalanceExercise("Test name", 20, 1, 3, 70, "None");
         assertEquals(balanceExercise.getType(), ExerciseType.BALANCE);
     }
