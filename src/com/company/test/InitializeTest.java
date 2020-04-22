4404ac2817054b89f138907cf653959ec643586e
Andrea Meyer <an-dr-ea93@hotmail.com>
2020-04-22 15:39:47 +0200

    5 new tests in the InitializerTest class

--- src/com/company/test/InitializeTest.java
+++ src/com/company/test/InitializeTest.java
@@ -2,6 +2,8 @@ package com.company.test;
 
 import com.company.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -10,10 +12,51 @@ import static org.junit.Assert.*;
 
 public class InitializeTest {
 
+    @BeforeClass
+    public static void setUp() {
+
+        Initialize.intitializeExercises();
+        Initialize.initializePrograms();
+        Initialize.initializePersons();
+
+    }
+
     @Test
+    public void test10_1() {
 
-     public void test10_1() {
+        assertEquals(Initialize.persons.size(), 8);
 
+    }
 
+    @Test
+    public void test10_2() {
+
+        assertEquals(Initialize.programs.size(), 7);
+
+    }
+
+    @Test
+    public void test10_3() {
+
+        assertEquals(Initialize.exercises.size(), 49);
+    }
+
+    @Test
+    public void test10_4() {
+
+        int burpeesIndex = Initialize.searchFunctionExercises("Burpees");
+        assertEquals(burpeesIndex, 9);
+        Exercise burpees = Initialize.exercises.get(burpeesIndex);
+        assertEquals(burpees.getName(), "Burpees");
+
+    }
+
+    @Test
+    public void test10_5() {
+
+        int batmanIndex = Initialize.searchFunctionPrograms("The Batman Program");
+        assertEquals(batmanIndex, 3);
+        Program theBatmanProgram = Initialize.programs.get(batmanIndex);
+        assertEquals(theBatmanProgram.getName(), "The Batman Program");
     }
 }
